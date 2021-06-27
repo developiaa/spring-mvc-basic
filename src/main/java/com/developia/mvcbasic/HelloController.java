41f83d20b6bac3b2cde705bc838a42d024ffc77d
developiaa <developiaa@gmail.com>
2021-06-27 15:55:45 +0900

    feat : Add template engine

--- src/main/java/com/developia/mvcbasic/HelloController.java
+++ src/main/java/com/developia/mvcbasic/HelloController.java
@@ -3,15 +3,22 @@ package com.developia.mvcbasic;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("hello")
     public String hello(Model model) {
         // 템플릿 뷰 엔진에서 파싱하여 보여줄수 있다.
         // viewResolver가 화면을 알아서 찾아서 처리함
         // resources:templates/ +{ViewName}+ .html
         model.addAttribute("data", "hello");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }

4cf5e429e4f4f5df6feb948f205236931ce7c073
developiaa <developiaa@gmail.com>
2021-06-27 16:03:38 +0900

    feat : Add api response

--- src/main/java/com/developia/mvcbasic/HelloController.java
+++ src/main/java/com/developia/mvcbasic/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -16,9 +17,42 @@ public class HelloController {
         return "hello";
     }
 
+    // template 방식
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam String name, Model model) {
+    public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    // api 방식 - 문자열 방식
+    // api 방식은 viewResolver 대신 HttpMessageConverter가 작동
+    // Http Accept Header와 컨트롤러 반환 타입들을 조합해서 결정됨(기본 문자인지 객체인지)
+    // 기본 문자 처리 StringHttpMessageConverter
+    @GetMapping("hello-spring")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name, Model model) {
+        return "hello " + name;
+    }
+
+    // api 방식 - 객체 방식 (json)
+    // 기본 객체 처리 MappingJackson2HttpMessageConverter
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }

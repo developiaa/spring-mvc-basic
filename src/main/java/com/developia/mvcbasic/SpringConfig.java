6871f385cac387798225170ef378b97326f0ec7d
developiaa <developiaa@gmail.com>
2021-06-30 23:41:56 +0900

    feat : Add h2 db with Jdbc

--- src/main/java/com/developia/mvcbasic/SpringConfig.java
+++ src/main/java/com/developia/mvcbasic/SpringConfig.java
@@ -1,14 +1,24 @@
 package com.developia.mvcbasic;
 
+import com.developia.mvcbasic.repository.JdbcMemberRepository;
 import com.developia.mvcbasic.repository.MemberRepository;
 import com.developia.mvcbasic.repository.MemoryMemberRepository;
 import com.developia.mvcbasic.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 // 직접 스프링 빈 객체로 등록하는 방법, @Service, @Repository와 동일
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }

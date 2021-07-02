62bcdc77467b2f83c995b37bf9bd21053f4b4b35
developiaa <developiaa@gmail.com>
2021-07-02 22:03:08 +0900

    feat : Add spring data jpa

--- src/main/java/com/developia/mvcbasic/SpringConfig.java
+++ src/main/java/com/developia/mvcbasic/SpringConfig.java
@@ -5,32 +5,35 @@ import com.developia.mvcbasic.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 // 직접 스프링 빈 객체로 등록하는 방법, @Service, @Repository와 동일
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-    private EntityManager em;
+//    private final DataSource dataSource;
+//    private EntityManager em;
+//
+//    public SpringConfig(DataSource dataSource, EntityManager em) {
+//        this.dataSource = dataSource;
+//        this.em = em;
+//    }
 
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
